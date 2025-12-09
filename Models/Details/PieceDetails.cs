8820561d3bab0dbe27f62fa7197c5573f0f92c66
sebbe <sebastian.gabrielsson03@gmail.com>
2025-12-09 14:51:30 +0100

    Added piece details

---
+++ Models/Details/PieceDetails.cs
@@ -0,0 +1,12 @@
+namespace OthelloProject.Models.Methods
+{
+	public class PieceDetails
+	{
+		public PieceDetails(){}
+
+		public int row { get; set; }
+		public int column  { get; set; }
+		public int UserID { get; set; }
+		public int GameID { get; set; }
+	}
+}
